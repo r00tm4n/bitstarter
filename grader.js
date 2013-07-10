2ff3a9fb98fa0211ae9b0a1d9aa5be1a14286b83
Ernesto Sitoe <nelitositoe@gmail.com>
2013-07-10 23:28:47 +0200

    Added url as a command line argument

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var TMPFILE = "tmpfile.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,12 +63,36 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+
+//We want to get inpput from an url
+var getContentFromUrlWriteToFile = function(tmpfile, url){
+    var content ='';
+    rest.get(url).on('complete', function(result){
+	if(result instanceof Error){
+            fs.writeFile(tmpfile, '');
+	} else {
+            fs.writeFile(tmpfile, result);
+	}
+    });
+    
+};
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = {};
+    if(program.file){
+	 checkJson = checkHtmlFile(program.file, program.checks);
+    } 
+    if(program.url){
+         getContentFromUrlWriteToFile(TMPFILE, program.url);
+         checkJson = checkHtmlFile(TMPFILE, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
